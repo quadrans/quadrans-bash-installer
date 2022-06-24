f0aacfe1e0c57da58f24488bfc54bfb90bb633ee
Piersandro Guerrera <piersandro@gmail.com>
2022-06-24 21:04:26 +0200

    Update v0.2 - Added node information and instructions at the end of the installation. Fixed node folder indication at the end of the installation

--- gqdc-winstaller.bat
+++ gqdc-winstaller.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Usage: open a Command Prompt or PowerShell on Windows and execute these commands:
-:: curl -# https://repo.quadrans.io/installer/gqdc-winstaller.bat -o gqdc-winstaller.bat
+:: curl -s https://repo.quadrans.io/installer/gqdc-winstaller.bat -o gqdc-winstaller.bat
 :: gqdc-winstaller.bat
 ::
 :: This is a command line tool to install Quadrans node
@@ -22,7 +22,7 @@ echo ^| ^| ^| ^| ^| ^| ^|/ _` ^|/ _` ^| `__/ _` ^| `_ \/ __^| ^|  \^| ^|/ _ \ / 
 echo ^| ^|_^| ^| ^|_^| ^| (_^| ^| (_^| ^| ^| ^| (_^| ^| ^| ^| \__ \ ^| ^|\  ^| (_) ^| (_^| ^|  __/
 echo  \__\_\\__,_^|\__,_^|\__,_^|_^|  \__,_^|_^| ^|_^|___/ ^|_^| \_^|\___/ \__,_^|\___^|
 echo:
-echo Welcome to Quadrans Node Installer v. 0.1 for Windows
+echo Welcome to Quadrans Node Installer v. 0.2 for Windows
 echo:
 echo This is a simple tool that allows to install a Go Quadrans binary for Windows.
 echo With easy steps it will download the node on your PC, create a wallet and enable mining.
@@ -52,5 +52,14 @@ echo by selecting the text with your pointer and double right click on your mous
 set /p NODE_WALLET="Wallet Address: "
 echo gqdc --mine --unlock 0x%NODE_WALLET%  --password password.txt --ethstats "%NODE_NAME%":QuadransStatsNetwork@status.quadrans.io:3000 --datadir . > gqdc-launcher.bat
 echo: 
-echo Quadrans Node Installation completed.
-echo Open Windows Explorer, go on %USERPROFILE% folder and double click on gqdc-launcher.bat file.
+echo Well done! Your Quadrans Node Installation has been completed. 
+echo: 
+echo To configure your node as Miner/Masternode please complete the registration on Quadrans Community from
+echo https://quadrans.io/get-started
+echo:
+echo Quadrans Node Information:
+echo - Name: %NODE_NAME%
+echo - QDC wallet: 0x%NODE_WALLET%
+echo: 
+echo Open Windows Explorer, go on %USERPROFILE%\QuadransNode folder and double click on gqdc-launcher.bat file to execute your Quadrans Node.
+echo If requested by your Firewall please Allow qgdc.exe to connect to the Internet.
